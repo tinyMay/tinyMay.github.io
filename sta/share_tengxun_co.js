9ba55276c0bd7d43fa8a7d0219765144bf8d71de
tinyMay <tiny_may@163.com>
2017-09-22 16:40:25 +0800

    Site updated: 2017-09-22 16:40:24

--- sta/share_tengxun_co.js
+++ sta/share_tengxun_co.js
@@ -25,23 +25,24 @@
       wx.error(function(res) {
         alert("微信分享配置错误");
       });
-      wx.closeWindow();
-      wx.getLocation({
-          type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-          success: function (res) {
-              var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-              var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-              var speed = res.speed; // 速度，以米/每秒计
-              var accuracy = res.accuracy; // 位置精度
-              alert("getLocation");
-          }
-      });
+      
       wx.ready(function() {
         alert("ready")
         var config = {title:data.title, desc:data.summary, link:data.url, imgUrl:data.pic, type:"", dataUrl:"", success:function() {
           data.callback && data.callback();
         }, cancel:function() {
         }};
+        wx.closeWindow();
+        wx.getLocation({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            success: function (res) {
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
+                alert("getLocation");
+            }
+        });
         wx.onMenuShareAppMessage(config);
         wx.onMenuShareQQ(config);
         wx.onMenuShareQZone(config);
